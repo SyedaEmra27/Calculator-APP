a62f9c60e164b2b471ad080e8fa5654c9f50a724
Syeda Emra <syedaemra@gmail.com>
2025-04-13 04:34:37 +0500

    Add files via upload

---
+++ Calculator App/app.js
@@ -0,0 +1,23 @@
+var inputElement = document.getElementById('inputField');
+function getValue(data){
+    
+    inputElement.value += data
+}
+function clrAll(){
+
+    inputElement.value = " ";
+}
+
+function square() {
+    inputElement.value = inputElement.value *  inputElement.value; 
+        
+}
+
+function deleteChar(){
+    inputElement.value = inputElement.value.slice(0 , -1)
+    
+}
+function equal() {
+    inputElement.value = eval(inputElement.value);
+
+}
